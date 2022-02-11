0f4832c93c19b2fa1652919722364fcaecac6645
German De Francesco <germandefrancesco@hotmail.com>
2022-02-10 21:58:03 -0300

    implement album delete

--- BlazorMusicCatalogCourse.Data/Services/AlbumService.cs
+++ BlazorMusicCatalogCourse.Data/Services/AlbumService.cs
@@ -12,9 +12,10 @@ namespace BlazorMusicCatalogCourse.Data.Services
             _context = context;
         }
 
-        public Task<bool> DeleteAlbum(Album album)
+        public async Task<bool> DeleteAlbum(Album album)
         {
-            throw new NotImplementedException();
+            _context.Remove(album);
+            return await _context.SaveChangesAsync() > 0;
         }
 
         public async Task<Album?> GetAlbum(int id)
